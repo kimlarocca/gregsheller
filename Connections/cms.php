e57b6851e91f04614186908af6dcae499e4ac59a
Kim LaRocca <kim@kimlarocca.com>
2020-05-13 14:33:56 -0400

    first commit

---
+++ Connections/cms.php
@@ -0,0 +1,25 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_cms = "localhost";
+$database_cms = "kim_4site";
+$username_cms = "kim_larocca";
+$password_cms = "Lotus18641864!";
+$cms = mysql_pconnect($hostname_cms, $username_cms, $password_cms) or trigger_error(mysql_error(),E_USER_ERROR); 
+$websiteID = 7;
+$idxLink = 'https://stellar.mlsmatrix.com/Matrix/public/IDX.aspx?idx=fd44113f';
+?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var $_Tawk_API={},$_Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/582b5dab73e3d85bf107f168/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
